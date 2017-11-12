db37d21b8cb2bed9b229854109bb79ec30fb45c9
Avi Flombaum <avi@flombaum.com>
2017-11-12 18:31:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ function lowerCaseDrivers(array){
 function nameToAttributes(names){
   // names =  ['Bobby Smith', 'Sammy Watkins']
   return names.map(function(name){
+
     debugger
     // name "Bobby Smith"
+
     let fName = name.split(" ")[0]
     let lName = name.split(" ")[1]
     return {firstName: fName, lastName: lName}
