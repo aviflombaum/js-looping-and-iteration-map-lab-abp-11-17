20ce759cb6b96fe93a39657bf1970ad0bb5358f5
Avi Flombaum <avi@flombaum.com>
2017-11-12 18:20:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ function lowerCaseDrivers(array){
   return array.map(function(e){
     return e.toLowerCase()
   })
-  
+
+}
+//
+// { firstName: 'Bobby',   lastName: 'Smith'     },
+
+function nameToAttributes(names){
+  // names =  ['Bobby Smith', 'Sammy Watkins']
+  return names.map(function(name){
+    // name "Bobby Smith"
+    let fName = name.split(" ")[0]
+    let lName = name.split(" ")[1]
+    return {firstName: fName, lastName: lName}
+  })
+  // [{firstName: "Bobby", lastName: "Smith"}, {firstName: "Sammy", lastName: "Watkins"}]
 }
